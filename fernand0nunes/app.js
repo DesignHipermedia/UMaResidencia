c386a297d25c60e82df1ce897ba786cb4e26c961
afonso.sousa10 <afonsosousa10@gmail.com>
2019-01-04 19:30:59 +0000

    update

--- fernand0nunes/app.js
+++ fernand0nunes/app.js
@@ -1,3 +1,4 @@
+//navbar motion
 const navSlide =() => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
@@ -5,16 +6,37 @@ const navSlide =() => {
     
     burger.addEventListener('click', () =>{
         //toggle Nav
-        nav.classList.toggle('nav-active');   
-            //Animate Links
+        nav.classList.toggle('nav-active');  
+
+        //Animate Links
         navLinks.forEach((link, index) => {
             if(link.style.animation) {
             link.style.animation = ''; 
             } else {
-                link.style.animation = 'navLinkFade 0.5s ease forwards ${index /7 + 0.3}s'
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.3}s`;
             }
         });
+        //burger animation
+        burger.classList.toggle('toogle');
     });
-
 }
 navSlide();
+//scroll image
+$(window).scroll(function () {
+    var scroll_position = $(window).scrollTop();
+    $('section').css({
+        'background-position-x': - scroll_position + 'px',
+    })
+})
+//background navbar
+$(window).on('scroll', function(){
+    if ($(window).scrollTop()){
+         $('nav').addClass('black');
+    }
+    else 
+    {
+        $('nav').removeClass('black');
+    }
+})
+
+
